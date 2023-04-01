70b27c06133e1f3f0ae25908b20f0c5b3880dbc6
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-04-01 11:49:09 +0300

    Initial Commit

---
+++ HW 2.06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.06
+//
+//  Created by Kasharin Mikhail on 01.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
