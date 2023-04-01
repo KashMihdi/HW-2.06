43e986263358e8a9b36ccd2283b678bce1a6c552
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-04-01 21:54:04 +0300

    add gradient for the second view controller

--- HW 2.06/WelcomeViewController.swift
+++ HW 2.06/WelcomeViewController.swift
@@ -13,9 +13,35 @@ final class WelcomeViewController: UIViewController {
     
     var userName: String!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setGradientBackgroundColor()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         greetingLabel.text = userName
     }
     
+    
+    private func setGradientBackgroundColor() {
+        let colorTop = UIColor(
+            red: 128 / 255,
+            green: 0 / 255,
+            blue: 128 / 255,
+            alpha: 1
+        ).cgColor
+        let colorBottom = UIColor(
+            red: 0 / 255,
+            green: 255 / 255,
+            blue: 255 / 255,
+            alpha: 1
+        ).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+        
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
